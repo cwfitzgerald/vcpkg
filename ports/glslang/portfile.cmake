88ad52bdc2662de55edb1aec6923fd5ab6a181f8
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-03-03 14:54:08 -0500

    [glslang] Add cmake targets for easy integration

--- ports/glslang/portfile.cmake
+++ ports/glslang/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
   REF b5b5f918c6b72d7cf2ee73641cc6c6ddb211ca70
   SHA512 ec0f7a23fa60457a481c7b3acf4c127c3bf898d23655d346aeafb304f74e798d632c83d676873f2c764d241de6dc4392cff8d6ce0ee509a4b74ee2233b01c008
   HEAD_REF master
+  PATCHES
+  	cmake-exports.patch
 )
 
 vcpkg_configure_cmake(
@@ -19,8 +21,9 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(RENAME "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/tools")
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+file(RENAME "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/tools")
 
 # Handle copyright
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/glslang)
